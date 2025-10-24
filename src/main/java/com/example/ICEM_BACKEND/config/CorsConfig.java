fcbbd797d292c8c33d5759033ec76cada75f85b1
Gaurav <122665090+Gaurav4344@users.noreply.github.com>
2025-10-24 17:39:16 +0530

    Updated Cors error

--- src/main/java/com/example/ICEM_BACKEND/config/CorsConfig.java
+++ src/main/java/com/example/ICEM_BACKEND/config/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
                                 "http://localhost:5173",
                                 "http://localhost:5174", // ✅ Added another allowed frontend port
                                 "http://localhost:3000", // ✅ Added another allowed frontend port
-                                "https://icemcms.vercel.app/" // ✅ Added another allowed frontend port
+                                "https://icemcms.vercel.app" // ✅ Added another allowed frontend port
                         )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
